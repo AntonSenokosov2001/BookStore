cf357b39574cb847ba0b9b522e0264279b84f9b5
Mihail Metelkin <mihey070@gmail.com>
2021-11-07 14:01:01 +0200

    Create Folders: Abstract, Entities and add files: Book, Publishing, Genre and theirs interfaces.

---
+++ BookStore/BookStore.Domain/Abstract/IPublishingRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using BookStore.Domain.Entities;
+
+namespace BookStore.Domain.Abstract
+{
+    interface IPublishingRepository
+    {
+        IEnumerable<Publishing> Publishes { get; }
+    }
+}
